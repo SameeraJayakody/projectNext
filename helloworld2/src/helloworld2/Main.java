8fdc8b5d4bcd65477b159a0605d2e37047448f3b
SameeraJayakody <jayakodymaduranga@gmail.com>
2020-02-24 23:36:19 -0800

    this is my first commit

---
+++ helloworld2/src/helloworld2/Main.java
@@ -0,0 +1,12 @@
+package helloworld2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("This is my first project at sliit");
+
+	}
+
+}
